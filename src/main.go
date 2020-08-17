ae842b08675462eb30260402c7f1a3d2da44aac7
rbobrovnikov <ruslan.bobrovnikov@gmail.com>
2020-08-18 01:15:37 +0300

    service initial logic change

--- src/main.go
+++ src/main.go
@@ -14,8 +14,11 @@ import (
 
 func main() {
 	logger.InitLogLevels(os.Stdout, os.Stdout, os.Stderr)
-
 	logger.Info.Printf(config.ServiceName+" started at %s\nBuild info: [ Date: %s | Tag: %s | Branch: %s | Commit: %s ]", config.ListenAddr, logger.BuildDate, logger.BuildTag, logger.BuildBranch, logger.BuildCommit)
+
+	metrics.RegisterMetrics()
+	http.Handle("/metrics", promhttp.Handler())
+	logger.Info.Fatal(http.ListenAndServe(config.ListenAddr, nil))
 
 	// Check Ejabberd API availability
 	ejabberdAPIStatusCode := 0
@@ -24,11 +27,11 @@ func main() {
 		reqBodyJSONEmpty := `{}`
 		_, ejabberdAPIStatusCode := httprequest.EjabberAPICommonRequest(httprequest.HTTPBaseParams{Schema: s, Host: h, Port: p, Token: t, Endpoint: "status", ReqBody: reqBodyJSONEmpty})
 		if ejabberdAPIStatusCode == 200 {
-			metrics.RegisterMetrics()
+			// metrics.RegisterMetrics()
 			metrics.RecordMetrics(config.Config())
-			http.Handle("/metrics", promhttp.Handler())
+			// http.Handle("/metrics", promhttp.Handler())
 			logger.Info.Printf("Ejabberd API is available. Ready to collect metrics!")
-			logger.Info.Fatal(http.ListenAndServe(config.ListenAddr, nil))
+			// logger.Info.Fatal(http.ListenAndServe(config.ListenAddr, nil))
 		}
 		time.Sleep(5 * time.Second)
 	}
